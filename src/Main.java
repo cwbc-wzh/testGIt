19e3a14215b60c0627cab5d1e06b912f427a3c51
cwbc-wzh <wzh965104472@163.com>
2018-05-05 19:03:42 +0800

    改变Main方法

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+        String A = new String("aaa");
+        System.out.println(A);
         System.out.println("Hello World!");
     }
 }
